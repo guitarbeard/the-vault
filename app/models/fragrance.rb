f7063d9244840a68bc42ad2d2277e4839c905f5f
Alex Medina <alxdna@gmail.com>
2023-06-30 19:36:59 -0700

    limits fragrance x and y values

--- app/models/fragrance.rb
+++ app/models/fragrance.rb
@@ -1,2 +1,8 @@
 class Fragrance < ApplicationRecord
+  MAX_X = 9
+  MAX_Y = 9
+  MIN_X = 0
+  MIN_Y = 0
+  validates :x, numericality: { less_than_or_equal_to: MAX_X, greater_than_or_equal_to: MIN_X }
+  validates :y, numericality: { less_than_or_equal_to: MAX_Y, greater_than_or_equal_to: MIN_Y }
 end
--- spec/models/fragrance_spec.rb
+++ spec/models/fragrance_spec.rb
@@ -11,5 +11,25 @@ RSpec.describe Fragrance, type: :model do
       fragrance = create :fragrance, x: 1, y: 1
       expect { create :fragrance, x: 1, y: 1 }.to raise_error ActiveRecord::RecordNotUnique
     end
+
+    it 'prevents saving of record with x value greater than 9' do
+      expect { create :fragrance, x: 10 }.to raise_error ActiveRecord::RecordInvalid
+      expect { create :fragrance, x: 9 }.not_to raise_error
+    end
+
+    it 'prevents saving of record with y value greater than 9' do
+      expect { create :fragrance, y: 10 }.to raise_error ActiveRecord::RecordInvalid
+      expect { create :fragrance, y: 9 }.not_to raise_error
+    end
+
+    it 'prevents saving of record with x value less than 0' do
+      expect { create :fragrance, x: -1 }.to raise_error ActiveRecord::RecordInvalid
+      expect { create :fragrance, x: 0 }.not_to raise_error
+    end
+
+    it 'prevents saving of record with y value less than 0' do
+      expect { create :fragrance, y: -1 }.to raise_error ActiveRecord::RecordInvalid
+      expect { create :fragrance, y: 0 }.not_to raise_error
+    end
   end
 end
