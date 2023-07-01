91bced3546cb355e3247320be4c9a1b34ec16f8a
Alex Medina <alxdna@gmail.com>
2023-06-30 19:14:24 -0700

    prevents saving of record with same x y values

--- spec/models/fragrance_spec.rb
+++ spec/models/fragrance_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Fragrance, type: :model do
       fragrance = create :fragrance
       expect(fragrance).to be_persisted
     end
+
+    it 'prevents saving of record with same x y values' do
+      fragrance = create :fragrance, x: 1, y: 1
+      expect { create :fragrance, x: 1, y: 1 }.to raise_error ActiveRecord::RecordNotUnique
+    end
   end
 end
